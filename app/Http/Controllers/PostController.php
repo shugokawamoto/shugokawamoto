c803335dd2ff638b36cd8c961838342c7895b9a7
shugokawamoto <shukawa24@gmail.com>
2023-03-06 14:54:27 +0900

    Procfile for Heroku

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\PostController;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
-    {
-        return $post->get();
-    }
+    //
 }
